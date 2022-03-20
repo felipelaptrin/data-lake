e7fa4ca969b6e518588017b8431ef2560c3712fe
felipe trindade <felipelaptrin@gmail.com>
2022-03-19 22:09:57 -0300

    deleted unused files

--- pipeline/ingestion.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-s3 = boto3.resource("s3")
-s3.meta.client.upload_file(
-    Filename="/home/felipe/Downloads/MICRODADOS_ENEM_2019.csv",
-    Bucket="datalake-flat-937168356724",
-    Key="raw-data/enem2019.csv",
-)
--- pipeline_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "[PIPELINE] INGESTION STARTING..."
-python3 ./pipeline/ingestion.py
-
-echo "[PIPELINE] PROCESSING STARTING..."
-python3 ./pipeline/processed.py 
